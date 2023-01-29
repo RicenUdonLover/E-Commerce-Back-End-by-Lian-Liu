16c3a469bb2aaac019dce2267fb370c480ba4c7b
Lian Liu <riceudon@gmail.com>
2023-01-29 14:56:23 -0800

    fixed some bugs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,29 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, { 
-  foreignKey: 'category_id',
- });
+Product.belongsTo(Category,
+  {
+    foreignKey: 'category_id',
+  });
 
 // Categories have many Products
-Category.hasMany(Product, { 
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-});
+Category.hasMany(Product,
+  {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+  });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { 
-  through: ProductTag, 
-  foreignKey: 'product_id',
-  as: 'product_tags'
-});
+Product.belongsToMany(Tag,
+  {
+    through: {
+      model: ProductTag,
+      foreignKey: 'product_id'
+    },
+    as: 'product_tags'
+  });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { 
-  through: ProductTag, 
-  foreignKey: 'tag_id',
-  as: 'tagged_products'
- });
+Tag.belongsToMany(Product,
+  {
+    through: {
+      model: ProductTag,
+    foreignKey: 'tag_id'
+  },
+    as: 'tagged_products'
+  });
 
 module.exports = {
   Product,
