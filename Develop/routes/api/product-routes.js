0eb65a1170e9f5945e786a0289bd92731827d821
Lian Liu <riceudon@gmail.com>
2023-01-29 15:52:16 -0800

    Found syntax errors in the template req.body in product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -41,7 +41,15 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
+  /* req.body should look like this...
+    {
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
+    }
+  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -104,7 +112,6 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-
 });
 
 router.delete('/:id', async (req, res) => {
--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
